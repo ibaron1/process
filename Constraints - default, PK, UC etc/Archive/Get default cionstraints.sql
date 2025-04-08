61cb8e6422709a8981d9052f4a92d4f6adcd2cf3
ibaron1 <ibaron1@msn.com>
2025-04-08 10:42:24 -0400

    default constraint
    get default constraints

---
+++ Constraints - default, PK, UC etc/Archive/Get default cionstraints.sql
@@ -0,0 +1,31 @@
+--all constraints
+SELECT 
+    t.name AS Table_Name,
+    c.name AS Column_Name,
+    dc.name AS Default_Constraint_Name,
+    dc.definition AS Default_Constraint_Definition
+FROM 
+    sys.tables t
+INNER JOIN 
+    sys.columns c ON t.object_id = c.object_id
+INNER JOIN 
+    sys.default_constraints dc ON c.default_object_id = dc.object_id
+WHERE 
+    t.is_ms_shipped = 0 -- Exclude system tables
+ORDER BY 
+    t.name, c.name;
+
+--specific constraint
+SELECT 
+    t.name AS Table_Name,
+    c.name AS Column_Name,
+    dc.name AS Default_Constraint_Name,
+    dc.definition AS Default_Constraint_Definition
+FROM 
+    sys.tables t
+INNER JOIN 
+    sys.columns c ON t.object_id = c.object_id
+INNER JOIN 
+    sys.default_constraints dc ON c.default_object_id = dc.object_id
+WHERE t.name = 'Address' and c.name = 'ModifiedDate' and dc.name = 'DF_Address_ModifiedDate's
+
