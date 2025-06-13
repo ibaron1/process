d821958ce88658cf300cb73b9c3a279661c55165
ibaron1 <ibaron1@msn.com>
2025-06-13 14:39:42 -0400

    view

--- script out view.sql
+++ script out view.sql
@@ -13,6 +13,28 @@ WHERE v.name = 'YourViewName'
   AND SCHEMA_NAME(v.schema_id) = 'dbo';
 
  --============ View Definition + Referenced Tables =============--
+ DECLARE @ViewSchema SYSNAME = 'dbo';
+DECLARE @ViewName SYSNAME = 'YourViewName';
+
+-- 1. Get the CREATE VIEW definition
+SELECT 
+    ViewDefinition = OBJECT_DEFINITION(OBJECT_ID(QUOTENAME(@ViewSchema) + '.' + QUOTENAME(@ViewName)))
+;
+
+-- 2. Get the referenced tables
+SELECT 
+    ReferencedSchema = referenced_schema_name,
+    ReferencedEntity = referenced_entity_name,
+    ReferencedType = referenced_minor_name, -- e.g., column if applicable
+    ReferencedObjectType = o.type_desc
+FROM sys.dm_sql_referenced_entities(
+        QUOTENAME(@ViewSchema) + '.' + QUOTENAME(@ViewName), 
+        'OBJECT'
+    ) d
+LEFT JOIN sys.objects o 
+    ON OBJECT_ID(QUOTENAME(d.referenced_schema_name) + '.' + QUOTENAME(d.referenced_entity_name)) = o.object_id
+WHERE is_ambiguous = 0;
+
 
   --================= Script All Views in a Schema ===--
   SELECT 
