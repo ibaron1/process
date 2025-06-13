c5e9d46242c1ba713ab893b77a66ddb711c6e52b
ibaron1 <ibaron1@msn.com>
2025-06-13 15:27:59 -0400

    view

--- script out view.sql
+++ script out view.sql
@@ -1,24 +1,21 @@
 
-DECLARE @ViewSchema SYSNAME = 'dbo';
-DECLARE @ViewName SYSNAME = 'YourViewName';
-
-SELECT 
-    definition = OBJECT_DEFINITION(OBJECT_ID(QUOTENAME(@ViewSchema) + '.' + QUOTENAME(@ViewName)))
-
---========================
-SELECT sm.definition
-FROM sys.views v
-JOIN sys.sql_modules sm ON v.object_id = sm.object_id
-WHERE v.name = 'YourViewName'
-  AND SCHEMA_NAME(v.schema_id) = 'dbo';
-
  --============ View Definition + Referenced Tables =============--
  DECLARE @ViewSchema SYSNAME = 'dbo';
 DECLARE @ViewName SYSNAME = 'YourViewName';
 
 -- 1. Get the CREATE VIEW definition
+-- Extract and split definition line-by-line
 SELECT 
-    ViewDefinition = OBJECT_DEFINITION(OBJECT_ID(QUOTENAME(@ViewSchema) + '.' + QUOTENAME(@ViewName)))
+    LineNumber = ROW_NUMBER() OVER (ORDER BY (SELECT NULL)),
+    Line = LTRIM(RTRIM(value))
+FROM (
+    SELECT definition
+    FROM sys.sql_modules sm
+    JOIN sys.objects o ON sm.object_id = o.object_id
+    JOIN sys.schemas s ON o.schema_id = s.schema_id
+    WHERE s.name = @ViewSchema AND o.name = @ViewName
+) AS src
+CROSS APPLY STRING_SPLIT(src.definition, CHAR(10));  -- Split by line feed
 ;
 
 -- 2. Get the referenced tables
@@ -35,6 +32,19 @@ LEFT JOIN sys.objects o
     ON OBJECT_ID(QUOTENAME(d.referenced_schema_name) + '.' + QUOTENAME(d.referenced_entity_name)) = o.object_id
 WHERE is_ambiguous = 0;
 
+--======================================================================================================================
+DECLARE @ViewSchema SYSNAME = 'dbo';
+DECLARE @ViewName SYSNAME = 'YourViewName';
+
+SELECT 
+    definition = OBJECT_DEFINITION(OBJECT_ID(QUOTENAME(@ViewSchema) + '.' + QUOTENAME(@ViewName)))
+
+--========================
+SELECT sm.definition
+FROM sys.views v
+JOIN sys.sql_modules sm ON v.object_id = sm.object_id
+WHERE v.name = 'YourViewName'
+  AND SCHEMA_NAME(v.schema_id) = 'dbo';
 
   --================= Script All Views in a Schema ===--
   SELECT 
